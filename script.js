07ee79d51876c11513de4bfbc60a92d1aedd2917
Sriyan-Nakka <sriyannakka@gmail.com>
2025-06-07 17:39:55 +0530

    version 0.3

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
+//* Form Variables
 const wordForm = document.querySelector("#wordForm");
 const enteredWord = document.querySelector("#enteredWord");
+const includeAudio = document.querySelector("#includeAudio");
 
 wordForm.addEventListener("submit", function (e) {
   e.preventDefault();
   enteredWord.setCustomValidity("");
-  if (/\s/.test(enteredWord.value)) {
-    enteredWord.setCustomValidity("No spaces and more than one word allowed.");
+  if (
+    enteredWord.value.includes(" ") ||
+    enteredWord.value.includes(",") ||
+    enteredWord.value.includes("!") ||
+    enteredWord.value.includes("@") ||
+    enteredWord.value.includes("#") ||
+    enteredWord.value.includes("$") ||
+    enteredWord.value.includes("%") ||
+    enteredWord.value.includes("^") ||
+    enteredWord.value.includes("&") ||
+    enteredWord.value.includes("*") ||
+    enteredWord.value.includes("(") ||
+    enteredWord.value.includes(")") ||
+    enteredWord.value.includes("-") ||
+    enteredWord.value.includes("_") ||
+    enteredWord.value.includes("+") ||
+    enteredWord.value.includes("=") ||
+    enteredWord.value.includes("{") ||
+    enteredWord.value.includes("}") ||
+    enteredWord.value.includes("[") ||
+    enteredWord.value.includes("]") ||
+    enteredWord.value.includes("|") ||
+    enteredWord.value.includes(":") ||
+    enteredWord.value.includes(";") ||
+    enteredWord.value.includes("'") ||
+    enteredWord.value.includes("<") ||
+    enteredWord.value.includes(">") ||
+    enteredWord.value.includes(".") ||
+    enteredWord.value.includes("?") ||
+    enteredWord.value.includes("/")
+  ) {
+    enteredWord.setCustomValidity(
+      "No spaces, numbers, special symbols and more than one word are allowed."
+    );
     enteredWord.reportValidity();
     return;
   }
+  console.log(includeAudio.checked);
 
   document.querySelector("#result").style.display = "flex";
   findWordInfo();
